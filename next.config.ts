f6068463b21a1f0c741a4b8f84437c0076c30ee4
dryjorse <azim.kojoev1987@gmail.com>
2025-07-12 21:59:51 +0600

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,69 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule: { test: { test: (param: string) => void } }) =>
+        rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /src/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /src/] },
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  experimental: {
+    turbo: {
+      loaders: {
+        ".svg": ["@svgr/webpack"],
+      },
+    },
+  },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+  reactStrictMode: false,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wemay.geekstudio.kg",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "http",
+        hostname: "wemay.geekstudio.kg",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "wemay.edu.kg",
+        port: "",
+        pathname: "/**/*",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
